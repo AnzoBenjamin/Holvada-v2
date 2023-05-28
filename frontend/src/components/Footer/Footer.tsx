370f0c46692671e5883ce31197db89a083636d58
AnzoBenjamin <anzobnjmn@gmail.com>
2023-05-28 02:26:01 -0700

    Finishing up the main section

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -2,9 +2,6 @@ import classes from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <div>
-        <p>Copyright &copy; 2023</p>
-      </div>
       <div className={classes["footer__link-container"]}>
         <a href="">ABOUT US</a>
         <a href="">SERVICES</a>
@@ -13,6 +10,9 @@ const Footer = () => {
         <a href="#"><img src="/facebook.png" alt="" className={classes["footer-icon"]}/></a>
         <a href="#"><img src="/instagram.png" alt=""  className={classes["footer-icon"]}/></a>
         <a href="#"><img src="/twitter.png" alt=""  className={classes["footer-icon"]}/></a>
+      </div>
+      <div>
+        <p className={classes["footer-copyright"]}>Copyright &copy;</p>
       </div>
     </footer>
   );
--- frontend/src/components/Header/Hero.tsx
+++ frontend/src/components/Header/Hero.tsx
@@ -4,23 +4,19 @@ import classes from "./Hero.module.scss";
 const Hero = () => {
   return (
     <div className={classes.hero}>
-        <div className={classes['hero__heading-are']}>
-
-      <h3 className={styles["heading-tertiary"]}>Welcome</h3>
-      <h1 className={styles["heading-primary"]}>
-        Where design meets development
-      </h1>
-        </div>
-      <div className={classes['hero__text-area']}>
+      <div className={classes["hero__heading-area"]}>
+        <h3 className={styles["heading-tertiary"]}>Welcome</h3>
+        <h1 className={styles["heading-primary"]}>
+          Where design meets development
+        </h1>
+      </div>
+      <div className={classes["hero__text-area"]}>
         <p>Choose the best</p>
         <p>Choose Holvada</p>
       </div>
       <div className={classes["hero-img"]}>
-      <img
-        src="/hero-img.png"
-        alt="Guy with gadgets"
-        />
-        </div>
+        <img src="/hero-img.png" alt="Guy with gadgets" />
+      </div>
     </div>
   );
 };
