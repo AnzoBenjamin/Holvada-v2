506fc94a60d7e1a8687262a9ebe46bb7efc006fd
AnzoBenjamin <anzobnjmn@gmail.com>
2023-06-11 04:54:37 -0700

    Finishing up

--- frontend/src/components/Main/cta.tsx
+++ frontend/src/components/Main/cta.tsx
@@ -1,20 +1,17 @@
-import classes from "./Cta.module.scss"
+import classes from './Cta.module.scss'
 import Button from '../../UI/Button'
 
-const Cta = () => {
-    const buttonHandler = ()=> {
-
-    }
+const cta = () => {
+    const buttonHandler = ()=>{}
   return (
-    <section className={classes["section-cta"]}>
+<section className={classes["section-cta"]}>
         <div className={classes["cta-card"]}>
             <h3>Sign up for free</h3>
             <h2>Start an exciting journey</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, eaque.</p>
             <Button text='Join us' className='' onClick={buttonHandler}/>
         </div>
-    </section>
-  )
+    </section>  )
 }
 
-export default Cta
+export default cta
