d59f8e4697cdd51483fb81c08bec18c47924f9ab
AnzoBenjamin <anzobnjmn@gmail.com>
2023-08-15 04:00:54 +0300

    Finishing up

--- frontend/src/components/Main/Performance/PerformanceHero.tsx
+++ frontend/src/components/Main/Performance/PerformanceHero.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import classes from "./PerformanceHero.module.scss";
 import HeroSlider from "../../../UI/HeroSlider";
 
-const PerformanceHero = () => {
+const PerformanceHero: React.FC = () => {
   const HeroData = [
     { imageURL: "performance.webp", text: "sing", tag: "sing", category: "sing"},
     { imageURL: "chess.webp", text: "play", tag: "sing", category: "sing" },
