20ba262902586a240154d2b3bc4ff31289ce1f00
AnzoBenjamin <anzobnjmn@gmail.com>
2023-08-20 02:27:14 +0300

    Social Media edits

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -6,20 +6,33 @@ const Footer = () => {
       <div className={classes["footer-bottom"]}>
         <p>Copyright &copy; All rights reserved</p>
         <div className={classes["footer--icon-area"]}>
-          <a href="info@holvada.com">
+          <a target="blank" href="mailto:info@holvada.com">
             <img src="/gmail.webp" alt="" className={classes.icon} />
           </a>
-          <a href="twitter.com">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/holvada"
+          >
             <img src="/twitter.webp" alt="Twitter" className={classes.icon} />
           </a>
-          <a href="whatsapp.com">
+
+          <a target="blank" href="https://api.whatsapp.com/send?phone=+256780827089&text=Hello%20from%20your%20website">
             <img src="/whatsapp.webp" alt="Whatsapp" className={classes.icon} />
           </a>
-          <a href="facebook.com">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://facebook.com/holvada"
+          >
+            {" "}
             <img src="/facebook.webp" alt="Facebook" className={classes.icon} />
           </a>
-          <a href="instagram.com">
-            <img
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://instagram.com/holvada"
+          >            <img
               src="/instagram.webp"
               alt="Instagram"
               className={classes.icon}
